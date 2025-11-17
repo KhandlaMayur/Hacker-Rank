a4fe74594518b1d9905e985c15b15de830586b00
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2025-11-17 19:30:19 +0530

    Create Jim and the Orders.py

---
+++ Jim and the Orders.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jimOrders' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts 2D_INTEGER_ARRAY orders as parameter.
+#
+
+def jimOrders(orders):
+    # Write your code here
+    customers = []
+    for i, (t, d) in enumerate(orders, start=1):
+        customers.append((t + d, i))
+    customers.sort()
+    return [idx for _, idx in customers]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    orders = []
+
+    for _ in range(n):
+        orders.append(list(map(int, input().rstrip().split())))
+
+    result = jimOrders(orders)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
