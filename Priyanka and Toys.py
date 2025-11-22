3d18ed5c87755c84eba0e99caafb972f296a66fc
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2025-11-22 23:28:05 +0530

    Create Priyanka and Toys.py

---
+++ Priyanka and Toys.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'toys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY w as parameter.
+#
+
+def toys(w):
+    # Write your code here
+    w.sort()
+    b=1
+    limit=w[0]+4
+    
+    for i in w:
+        if (i > limit):
+            b += 1
+            limit = i + 4
+    return b
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    w = list(map(int, input().rstrip().split()))
+
+    result = toys(w)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
