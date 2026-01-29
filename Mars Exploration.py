e53db91af23ea449161c53ad427e382a73b32cef
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-01-29 11:19:17 +0530

    Create Mars Exploration.py

---
+++ Mars Exploration.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    # count = 0
+    # for i in range(len(s)):
+    #     expected = i % 3
+    #     if(s[i] != expected):
+    #         count += 1
+    # return count
+    
+    count = 0
+    for i in range(len(s)):
+        if (i % 3 == 0 and s[i] != 'S'):
+            count += 1 
+        elif(i % 3 == 1 and s[i] != 'O'):
+            count += 1
+        elif(i % 3 == 2 and s[i] != 'S'):
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
