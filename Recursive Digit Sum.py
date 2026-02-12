0a130033d4fc16babdb207d95bda2f11702b1812
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-02-12 10:29:29 +0530

    Add superDigit function for recursive digit sum
    Implement superDigit function to calculate the recursive digit sum based on input string and integer.

---
+++ Recursive Digit Sum.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    # Write your code here
+    intial_sum = sum(int(d) for d in n)
+    total = intial_sum * k
+    while(total >= 10):
+        total = sum(int(d) for d in str(total))
+    return total
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
